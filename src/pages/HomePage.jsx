75ff24f11b52acf425cd615577e8a323072eda8e
Deeksha <deekshasharma10782@gmail.com>
2025-05-10 23:13:45 +0530

    change 6

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../Home/Header';
-import HeroBooking from '../Home/HeroBooking';
+// import HeroBooking from '../Home/HeroBooking';
 import WhyAffluence from '../Home/WhyAffluence';
 import Destinations from '../Home/Destinations';
 import BlogSection from '../Home/BlogSection';
@@ -10,7 +10,7 @@ export default function HomePage() {
   return (
     <div className="bg-gray-50 font-sans text-gray-900">
       <Header />
-      <HeroBooking />
+      {/* <HeroBooking /> */}
       <div className="max-w-6xl mx-auto px-4">
         <WhyAffluence />
         <Destinations />
